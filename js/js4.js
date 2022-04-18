42febcbc15e12e329a37028f925698d6046a13e3
Di26m <express_musik@mail.ru>
2022-04-18 21:17:34 +1000

    18.04.2022

---
+++ js/js4.js
@@ -0,0 +1,6 @@
+// var num;
+// num = 5;
+
+var num=5;
+num = 7;
+console.log("переменная:  " + num + ".");
---
+++ js/js5.js
@@ -0,0 +1,15 @@
+var num_1 = 5;
+var num_2 = 15;
+
+console.log("Результат:" + (num_1 - num_2));
+
+var num_3 = 5;
+num_3 ++;
+console.log("Результат:" + num_3);
+
+var str_1 = Number ("12");
+var str_2 = Number ("2");
+console.log("Результат:" + (str_1 + str_2));
+
+console.log("Math: " + Math.PI);
+console.log("Math: " + Math.min(4, 6, -8, 0, 10));
