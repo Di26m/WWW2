a191909738dcb98b5c4e8c3857b01b14668fe31e
Di26m <express_musik@mail.ru>
2022-05-23 23:39:34 +1000

    23.05.2022

---
+++ js/js6.2.js
@@ -0,0 +1,14 @@
+var stroka = "word1";
+
+switch (stroka) {
+  case "4":
+  console.log ("Переменная со значением 4");
+  case "45":
+  console.log ("Переменная со значением 45");
+  case "word":
+  console.log ("Переменная со значением word");
+    break;
+  default:
+  console.log ("default")
+
+}
---
+++ js/js6.js
@@ -0,0 +1,16 @@
+var number = 15;
+var isHasHouse = true;
+
+if (number == 15 && isHasHouse == true ) {
+  console.log("Ok");
+  console.log("Ok!");
+} else if (number < 10) {
+  console.log("Ok!");
+} else if (number == 7) {
+  console.log("7");
+} else if (number > 15) {
+  console.log(">= 15");
+}
+else {
+  console.log("Else!");
+}
