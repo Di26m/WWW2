ccd09d55f609b6c2b8e8e0663f09baaf37a121de
Di26m <express_musik@mail.ru>
2022-06-30 23:53:33 +1000

    30/06/2022 2

--- pythonParcing/lesson1/lesson1.py
+++ pythonParcing/lesson1/lesson1.py
@@ -1,15 +1,63 @@
-d(sys)
-import locale
-sys.setdefaultencoding(locale.getpreferredencoding())efaultencoding(‘utf-8′)t sys
-reload(sys)
+# -*- coding: utf-8 -*-
+
 # pip install beautifulsoup4 lxml
 from bs4 import BeautifulSoup
 
-with open ('blank/index.html') as file:
+with open ('blank/index.html','r', encoding= 'utf-8') as file:
     src = file.read()
 
 soup = BeautifulSoup(src, "lxml")
 
-title = soup.title
-print(title)
-print(title.text)
+# title = soup.title
+# print(title)
+# print(title.text)
+# print('Тут могла бы быть ваша реклама')
+# .find() .find_all()
+# page_h1 = soup.find("h1")
+# print(page_h1)
+#
+# page_all_h1 = soup.find_all('h1')
+# print(page_all_h1)
+# for item in page_all_h1:
+#     print(item.text)
+
+# user_name = soup.find("div", class_="user__name")
+# print(user_name.text.strip())
+
+# user_name = soup.find(class_="user__name").find("span").text
+# print(user_name)
+
+# user_name = soup.find("div", {"class": "user__name", "id": "aaa"}).find("span").text
+# print(user_name)
+
+# find_all_spans_in_user_info = soup.find(class_="user__info").find_all("span")
+# print(find_all_spans_in_user_info)
+
+# for item in find_all_spans_in_user_info:
+#     print(item.text)
+
+# print(find_all_spans_in_user_info[0])
+# print(find_all_spans_in_user_info[2].text)
+
+# social_links = soup.find(class_="social__networks").find("ul").find_all("a")
+# print(social_links)
+
+# all_a = soup.find_all("a")
+# print(all_a)
+# for item in all_a:
+#     item_text = item.text
+#     item_url= item.get('href')
+#     print(f"{item_text}: {item_url} ")
+
+#------  .find_parent()  .find_parents()---------
+
+# post_div = soup.find(class_="post__text").find_parent()
+# print(post_div)
+
+# post_div = soup.find(class_="post__text").find_parent("div", "user__post")
+# print(post_div)
+
+# post_divs = soup.find(class_="post__text").find_parents()
+# print(post_divs)
+
+#
