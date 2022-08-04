27ab0e9359a5839901c2bb3f2b9e7fae00b25639
Di26m <express_musik@mail.ru>
2022-08-04 23:05:09 +1000

    04/08/2022

--- pythonParcing/lesson3/test.py
+++ pythonParcing/lesson3/test.py
@@ -7,11 +7,7 @@ from gtts import gTTS
 import re
 
 
-# for root, dirs, files in os.walk(input("ВВедите путь")):
-#     for file in files:
-#         if file.endswith(".fb2"):
-#             file_path = os.path.join(root, file)
-#             print(os.path.join(root, file))
+
 def Fb2_to_mp3(file_path=input("ВВедите путь"), language='ru'):
     os.chdir(file_path)
     for file_path in glob.glob("*.fb2"):
